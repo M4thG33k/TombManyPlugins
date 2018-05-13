2cb6a56cc1dda935689973b505ec29e6e6d3e4d1
= <pepethemathgeek@gmail.com>
2018-05-13 15:01:10 -0500

    Techguns - implemented special inventory

--- TechGuns/src/main/java/com/m4thg33k/tombmanyplugins_techguns/TombManyPluginsTechguns.java
+++ TechGuns/src/main/java/com/m4thg33k/tombmanyplugins_techguns/TombManyPluginsTechguns.java
@@ -1,10 +1,23 @@
 package com.m4thg33k.tombmanyplugins_techguns;
 
 import com.m4thg33k.tombmanygraves.api.events.EventRegisterSpecialInventory;
+import com.m4thg33k.tombmanygraves.api.inventory.ISpecialInventory;
+import com.m4thg33k.tombmanygraves.api.inventory.SpecialInventoryHelper;
+import com.m4thg33k.tombmanygraves.api.inventory.TransitionInventory;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import techguns.capabilities.TGExtendedPlayer;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod(
         modid = TombManyPluginsTechguns.MODID,
@@ -28,6 +41,79 @@ public class TombManyPluginsTechguns {
 
     @SubscribeEvent
     public void registerSpecialInventories(EventRegisterSpecialInventory e) throws Exception{
+        e.registerSpecialInventory(new SpecialInventoryTechguns());
+    }
+
+    class SpecialInventoryTechguns implements ISpecialInventory{
+        @Override
+        public String getUniqueIdentifier() {
+            return "TechgunsInventory";
+        }
+
+        @Override
+        public int getPriority() {
+            return 0;
+        }
+
+        @Override
+        public boolean isOverwritable() {
+            return true;
+        }
+
+        @Override
+        public boolean pregrabLogic() {
+            return true;
+        }
+
+        @Override
+        public NBTBase getNbtData(EntityPlayer player) {
+            return SpecialInventoryHelper.getTagListFromIInventory(TGExtendedPlayer.get(player).getTGInventory());
+        }
+
+        @Override
+        public void insertInventory(EntityPlayer player, NBTBase compound, boolean shouldForce) {
+            if (compound instanceof NBTTagList){
+                TransitionInventory graveItems = new TransitionInventory((NBTTagList)compound);
+                IInventory currentItems = TGExtendedPlayer.get(player).getTGInventory();
+
+                for (int i=0; i<graveItems.getSizeInventory(); i++){
+                    ItemStack graveItem = graveItems.getStackInSlot(i);
+                    if (!graveItem.isEmpty()){
+                        ItemStack playerItem = currentItems.getStackInSlot(i).copy();
+
+                        if (playerItem.isEmpty()){
+                            currentItems.setInventorySlotContents(i, graveItem);
+                        } else if (shouldForce){
+                            currentItems.setInventorySlotContents(i, graveItem);
+                            SpecialInventoryHelper.dropItem(player, playerItem);
+                        } else {
+                            SpecialInventoryHelper.dropItem(player, graveItem);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        @Nonnull
+        @Override
+        public List<ItemStack> getDrops(NBTBase compound) {
+            if (compound instanceof NBTTagList){
+                return (new TransitionInventory((NBTTagList)compound)).getListOfNonEmptyItemStacks();
+            } else {
+                return new ArrayList<ItemStack>();
+            }
+        }
+
+        @Override
+        public String getInventoryDisplayNameForGui() {
+            return "Techguns";
+        }
+
+        @Override
+        public int getInventoryDisplayNameColorForGui() {
+            return 0;
+        }
     }
 
 }
